d08ea3894f11bad4b20cd1345faf2baf3b19532d
Vanessa Toro <vanetoro84@gmail.com>
2018-05-19 21:02:43 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,8 +1,10 @@
+require 'pry'
 class Dog
   attr_accessor :name, :breed, :age
   @@all=[]
 
   def initialize(name, breed, age)
+    binding.pry
     name = @name,
     breed = @breed,
     age = @age,
