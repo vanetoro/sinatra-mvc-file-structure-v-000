7559d658d74782a47f7c91e96677bf124981f9f8
Vanessa Toro <vanetoro84@gmail.com>
2018-05-19 21:02:19 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -3,9 +3,9 @@ class Dog
   @@all=[]
 
   def initialize(name, breed, age)
-    name = @name
-    breed = @breed
-    age = @age
+    name = @name,
+    breed = @breed,
+    age = @age,
     @@all << self
   end
 
