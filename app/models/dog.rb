5990897459ee34b2db8d174fc3a1d124de65e705
Vanessa Toro <vanetoro84@gmail.com>
2018-05-19 21:00:17 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,7 +1,12 @@
 class Dog
   attr_accessor :name, :breed, :age
   @@all=[]
-
+  def initialize(name, breed, age)
+    @name = :name
+    @breed = :breed
+    @age = :age
+  end
+    
   def self.all
     @all
   end
